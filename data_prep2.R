7e38df2f1d78fdc3013aad670e49abff14d2642b
ddanieltan <danieltan.singapore@gmail.com>
2016-07-27 00:06:30 +0800

    Cleaned up code for visualizations

--- data_prep2.R
+++ data_prep2.R
@@ -18,6 +18,23 @@ test <- merge(x=dfTestTmp, y=dfFeatures, all.x=TRUE)
 train$Date <- as.Date(train$Date)
 test$Date <- as.Date(test$Date)
 
+#There are a total of 45 unique store numbers from 1 to 45.
+#There are a total of 81 unique dept numbers from 1 to 99.
+#Creating a df of unique store and dept pairs.
+unique_pairs <- unique(train[,c("Store","Dept")])
+
+#Creating visualizations of the weekly sales for each store-dept pair
+for (i in 1:nrow(unique_pairs)){
+  new_df <- filter(train,train$Store==unique_pairs[i,1],train$Dept==unique_pairs[i,2])
+  name_of_file <- paste("visualizations/store",unique_pairs[i,1],"_dept",unique_pairs[i,2],".png",sep="")
+  png(filename=name_of_file)
+  plot(new_df$Weekly_Sales~new_df$Date,type="l",
+   main=name_of_file,
+   xlab="Date",
+   ylab = "Weekly Sales")
+  dev.off()
+}
+
 #Create a subset of just Weekly Sales over 52 weeks for each store-dept pair
 s1d1 <- train %>%
   filter(Store == 1, Dept == 1) %>%
@@ -32,57 +49,13 @@ plot.ts(s1d1.ts,
         xlab="Time",
         ylab="Weekly Sales")
 
-#There are a total of 45 unique store numbers from 1 to 45.
-#There are a total of 81 unique dept numbers from 1 to 99.
-#Creating a df of unique store and dept pairs.
-unique_pairs <- unique(train[,c("Store","Dept")])
-
-for (i in unique_pairs){
+for (i in 1:nrow(unique_pairs)){
   temp.df <- train %>%
-    filter(Store==unique_pairs$Store, Dept==unique_pairs$Dept) %>%
+    filter(Store==unique_pairs[i,1], Dept==unique_pairs[i,2]) %>%
     select(5) #Weekly_sales
-  name_of_file <- paste("visualizations/store",unique_pairs$Store,"_dept",unique_pairs$Dept,".png",sep="")
+  name_of_file <- paste("visualizations/store",unique_pairs[i,1],"_dept",unique_pairs[i,2],".png",sep="")
   temp.ts <- ts(temp.df,frequency=52)
   png(filename=name_of_file)
   plot.ts(temp.ts)
   dev.off()
 }
-
-#Creating visualizations of the weekly sales for each store-dept pair
-for (i in unique_pairs){
-  temp_store <- unique_pairs[i,]$Store
-  temp_dept <- unique_pairs[i,]$Dept
-  #new_df <- filter(train,train$Store==temp_store,train$Dept==temp_dept)
-  new_df <- subset(train,Store==temp_store,Dept==temp_dept)
-  name_of_file <- paste("visualizations/store",temp_store,"_dept",temp_dept,".png",sep="")
-  png(filename=name_of_file)
-  plot(new_df$Weekly_Sales~new_df$Date,type="l",
-       main=name_of_file,
-       xlab="Date",
-       ylab = "Weekly Sales")
-  dev.off()
-}
-
-for (i in 1:45){
-  for (j in dept_num_list){
-    new_df <- filter(train,train$Store==i,train$Dept==j)
-    name_of_file <- paste("visualizations/store",i,"_dept",j,".png",sep="")
-    
-    png(filename=name_of_file)
-    plot(new_df$Weekly_Sales~new_df$Date,type="l",
-         main=name_of_file,
-         xlab="Date",
-         ylab = "Weekly Sales")
-    dev.off()
-  }
-}
-
-###For reference
-#df2010 <- train[train$Date <= "2011-01-01",]
-#dfstore1 <- train[train$Store == 1,]
-#s1d1 <- train %>%
-#  filter(train$Store==1,train$Dept==1)
-#png(filename = 'visualizations/test.png')
-#plot(s1d1$Weekly_Sales~s1d1$Date,type="l")
-#dev.off()
-
