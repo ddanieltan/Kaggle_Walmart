25434a91b6575c6501dd0a38ddb098c5890a6346
ddanieltan <danieltan.singapore@gmail.com>
2016-08-08 00:33:58 +0800

    Updated forecast methods

--- forecast2.R
+++ forecast2.R
@@ -1,2 +1,12 @@
 library(dplyr)
 library(forecast)
+
+test.dates <- unique(test$Date)
+num.test.dates <- length(test.dates)
+all.stores <- unique(test$Store)
+num.stores <- length(all.stores)
+test.depts <- unique(test$Dept)
+#reverse the depts so the grungiest data comes first
+test.depts <- test.depts[length(test.depts):1]
+forecast.frame <- data.frame(Date=rep(test.dates, num.stores),
+                             Store=rep(all.stores, each=num.test.dates))
--- main.R
+++ main.R
@@ -1,4 +1,6 @@
 source('data_prep3.R')
+source('forecast2.R')
 
 train <- read.train()
 test <- read.test()
+
