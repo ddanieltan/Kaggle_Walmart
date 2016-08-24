1bbf5d1008922168f442b89d2f8c7a1cf1b083f7
ddanieltan <danieltan.singapore@gmail.com>
2016-08-24 20:46:53 +0800

    Added rmd document

--- rough.R
+++ rough.R
@@ -8,7 +8,6 @@ test <- read.test()
 train <- select(train,Store,Dept,Date,Weekly_Sales)
 melt.df <- melt.data.frame(train, id.vars='Date')
 
-
 rough <- read.csv(file='data/rough.csv')
 rough$Date <- as.Date(rough$Date, format="%d/%m/%Y")
 rough <- tbl_df(rough)
@@ -24,16 +23,27 @@ train.rmna<- read.csv(file='data/train.csv',colClasses = cls, na.strings = 0)
 train.rmna<-tbl_df(train.rmna)
 
 
+#select unique store-depts that appear in test set
+unique.test.pairs <- unique(test[,c("Store","Dept")])
+unique.test.pairs<-mutate(unique.test.pairs,id=paste(Store,"_",Dept,sep="")) #3169
+
+col.num <- which(colnames(mts) %in% unique.test.pairs$id) #3158
+mts.test.pairs <-mts[,col.num] #3158
+
+#check for NAs
+sapply(mts.test.pairs, function(x) sum(is.na(x)))
+
+#removing NAs from mts.test.pairs
+mts.rmna <- mts.test.pairs[, colSums(is.na(mts.test.pairs)) == 0] #2660
+
+#Performing TSClust
 library(TSClust)
 tsdist <- diss(mts.rmna, "ACF", p=0.05) #this takes way too long
-hc <- hclust(tsdist)
-plot(hc)
 
-#to find sum of NAs in collumns
-sapply(mts.rmna, function(x) sum(is.na(x)))
+#Performing correlation analysis
+mts.cor <- cor(mts.rmna) #2660x2660 matrix
+hc<-hclust(mts.cor) #Error in if (is.na(n) || n > 65536L) stop("size cannot be NA nor exceed 65536")
 
-#removing NAs from mts
-mts.rmna <- mts[, colSums(is.na(mts)) == 0]
-mts.rmna <- subset(mts.rmna, select=-Date)
-
-
+#trying to use corrplot to visualize mts.cor
+library(corrplot)
+mts.corrplot<- corrplot(mts.cor,method="square",order="hclust")
